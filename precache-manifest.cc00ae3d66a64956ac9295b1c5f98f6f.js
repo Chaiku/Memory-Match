15b2a8a806ed7323b880fefc581cc890e832a14c
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-06-12 12:46:38 -0400

    Updates

---
+++ precache-manifest.cc00ae3d66a64956ac9295b1c5f98f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81159d23d04749b1f08f81cd574591fa",
+    "url": "/memory-match/index.html"
+  },
+  {
+    "revision": "4c6394900f01cdaca471",
+    "url": "/memory-match/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/memory-match/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4c6394900f01cdaca471",
+    "url": "/memory-match/static/js/main.5b79f80d.chunk.js"
+  },
+  {
+    "revision": "8019f833348ae71757da",
+    "url": "/memory-match/static/js/runtime~main.951af858.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-match/static/media/logo.5d5d9eef.svg"
+  }
+]);
